a75579db864e07565aaf4afd2baa5ae352c58bb4
Karen <47039819+kalilamali@users.noreply.github.com>
2022-08-11 14:11:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ import myfunctions
 import streamlit as st
 import pandas as pd
 
+
+# Main
 st.title('EFSA FoodEx2 code meaning')
 st.write('Created by 👩🏻‍💻 [kalilamali](https://github.com/kalilamali)')
 st.caption('This program takes an EFSA FoodEx2 code and returns its meaning.\
@@ -18,10 +20,8 @@ st.caption('This program takes an EFSA FoodEx2 code and returns its meaning.\
         https://www.wikidata.org/wiki/Property:P4637')
 st.caption('Remember: An EFSA FoodEx2 code has 5 letters. For example: A02XN')
 
-
 code = st.text_input('Write the code here!')
 if code:
     meaning = myfunctions.get_meaning_foodex2_code(code.strip())
     
     st.write(f'Meaning: {meaning}')
-
